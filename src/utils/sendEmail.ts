3ac592bcf8ec53980c9a0e6430ec6636ddf3a763
CaioPedras <caio.vp@gmail.com>
2022-09-19 17:53:10 -0300

    prepare for delpoy

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -3,14 +3,18 @@ import { findAllUsers } from "../repositories/authRepository";
 
 export async function sendEmail(data: string) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+  try {
+    const users = await findAllUsers();
+    const message = {
+      to: users,
+      from: "caio.vp@gmail.com",
+      subject: "Prova Adicionada Repoprovas",
+      text: data,
+      html: `<em>${data}</em>`,
+    };
 
-  const users = await findAllUsers();
-  const message = {
-    to: users,
-    from: "caio.vp@gmail.com",
-    subject: "Prova Adicionada Repoprovas",
-    text: data,
-    html: `<em>${data}</em>`,
-  };
-  await sgMail.send(message);
+    await sgMail.send(message);
+  } catch (err) {
+    console.log(err);
+  }
 }
